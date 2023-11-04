89340f71f9b5be5e36255e4891764c4e16a2555f
Borhan Uddin <68281712+wapborhan@users.noreply.github.com>
2023-11-04 11:06:44 +0600

    Upload

---
+++ README.md
@@ -0,0 +1,10 @@
+# momentum_parody
+Make a clone of momentum chrome extension which has 3M+ active users
+
+Hello!
+There are a few thing you need to do in order to succefully run this chrome extension!
+
+STEP 1: Change image url in homepage.html to a local image or a link, and I have worked out that the size that works the best is 1600 px by 787 px       
+
+
+STEP 2: Change name from John Doe to your name, this change won't affect the working of the extension if not executed
---
+++ js/random_roast.js
@@ -0,0 +1,47 @@
+const roasts = [
+  "Is your drama going to an intermission soon?",
+  "It’s a shame you can’t Photoshop your personality.",
+  "Jealousy is a disease. Get well soon, bitch! ",
+  "If I had a face like yours I’d sue my parents.",
+  "I forgot the world revolves around you. My apologies! How silly of me.",
+  "Hold still. I’m trying to imagine you with a personality.",
+  "Don’t be ashamed of who you are. That’s your parents’ job.",
+  "If you’re going to be two-faced, at least make one of them pretty.",
+  "Oops, my bad. I could’ve sworn I was dealing with an adult.",
+  "I would smack you, but I’m against animal abuse.",
+];
+
+var roast = document.getElementById("roasts");
+roast.innerHTML = roasts[Math.floor(Math.random() * roasts.length)];
+
+function updateTime() {
+  var today = new Date();
+  if (today.getMinutes() < 10) {
+    var time = today.getHours() + ":0" + today.getMinutes();
+  } else if (today.getMinutes() >= 10) {
+    var time = today.getHours() + ":" + today.getMinutes();
+  }
+  //   var t = document.getElementById("currentTime");
+  //   t.innerHTML = time;
+
+  const greet1 = "Good morning,Borhan Uddin.";
+  const greet2 = "Good afternoon, Borhan Uddin.";
+  const greet3 = "Good evening, Borhan Uddin.";
+  const greet4 = "Good Night, Borhan Uddin.";
+
+  var greeting = document.getElementById("greetingHeading");
+
+  if (today.getHours() > 4 && today.getHours() < 12) {
+    greeting.innerHTML = greet1;
+  }
+  if (today.getHours() >= 12 && today.getHours() < 16) {
+    greeting.innerHTML = greet2;
+  }
+  if (today.getHours() >= 16 && today.getHours() <= 24) {
+    greeting.innerHTML = greet3;
+  }
+  if (today.getHours() >= 17 && today.getHours() <= 24) {
+    greeting.innerHTML = greet4;
+  }
+}
+setInterval(updateTime, 10);
